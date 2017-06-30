6bd7e16a86b42018aeba10f76177e3dd5868a095
j0hnnyM33rkat <j0hnnyM33rkat@github.com>
2017-06-30 09:43:17 +0100

    ...

---
+++ src/main/scala/org/garagescience/deeplearning/nosgd/XOR/examples/Helpers.scala
@@ -0,0 +1,13 @@
+package org.garagescience.deeplearning.nosgd.XOR.examples
+
+object Helpers {
+
+  def assymetricalCoin(p: Double): Boolean = (math.random < p)
+
+  def time(f: => Unit)={
+    val s = System.currentTimeMillis
+    f
+    print("execution time in seconds: ")
+    println((System.currentTimeMillis - s) * 1.0 / 1000.0)
+  }
+}
