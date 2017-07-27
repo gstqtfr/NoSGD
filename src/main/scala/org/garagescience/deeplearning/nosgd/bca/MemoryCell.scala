5832063bd79d59ccfba01e43864ada460960e0af
j0hnnyM33rkat <j0hnnyM33rkat@github.com>
2017-07-27 15:39:38 +0100

    ...

--- src/main/scala/org/garagescience/deeplearning/nosgd/bca/MemoryCell.scala
+++ src/main/scala/org/garagescience/deeplearning/nosgd/bca/MemoryCell.scala
@@ -1,5 +1,9 @@
 package org.garagescience.deeplearning.nosgd.bca
 
+import scala.language.higherKinds
+import scala.language.implicitConversions
+
+
 // TODO: we need a memoery cell to provide the momentum function
 // TODO: this simply stores the previous selected candidate from
 // TODO: the last evaluation & adds it in proportion to a coeff.
@@ -18,4 +22,12 @@ Momentum αα is used to diminish the fluctuations in weight changes over consec
 where E(w)E(w) is the error function, ww - the vector of weights, ηη - learning rate.
  */
 
-// TODO: so let's implement a Seq to hold the previous vals 
+// TODO: so let's implement a type-parameterised collection to hold the previous vals
+
+trait Memory[M[_], +A] {
+
+  def alpha: A
+  def memory[M[A]]
+  // def memory[U]: M[U]
+
+}
